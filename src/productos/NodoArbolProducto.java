33bdddbdb425cccd8f563a70df7213fa3f233f0f
KRISTHEL BADILLA CERDAS <kbadillac@ucenfotec.ac.cr>
2026-07-18 21:08:23 -0600

    Se agregan las clases ArbolProductos y NodoArbolProductos

---
+++ src/productos/NodoArbolProducto.java
@@ -0,0 +1,43 @@
+package productos;
+
+public class NodoArbolProducto {
+
+    private Producto producto;
+    private NodoArbolProducto izquierdo;
+    private NodoArbolProducto derecho;
+
+    public NodoArbolProducto(Producto producto) {
+        this.producto = producto;
+        izquierdo = null;
+        derecho = null;
+    }
+
+    //Getter
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public NodoArbolProducto getIzquierdo() {
+        return izquierdo;
+    }
+
+    public NodoArbolProducto getDerecho() {
+        return derecho;
+    }
+
+    //Setter
+
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+
+    public void setIzquierdo(NodoArbolProducto izquierdo) {
+        this.izquierdo = izquierdo;
+    }
+
+    public void setDerecho(NodoArbolProducto derecho) {
+        this.derecho = derecho;
+    }
+}
